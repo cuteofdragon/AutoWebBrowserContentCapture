e9545923f8ddac1141d17260df40029d72296746
cuteofdragon <cuteofdragon@gmail.com>
2019-09-24 18:04:50 +0800

    Initial

---
+++ AutoWebBrowserContentCapture/MainForm.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace AutoWebBrowserContentCapture
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.submitButton = new System.Windows.Forms.Button();
+            this.urlTextBox = new System.Windows.Forms.TextBox();
+            this.fontDialog1 = new System.Windows.Forms.FontDialog();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.removeImageCheckBox = new System.Windows.Forms.CheckBox();
+            this.removeHyperlinkCheckBox = new System.Windows.Forms.CheckBox();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft JhengHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(12, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "網址：";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.removeHyperlinkCheckBox);
+            this.panel1.Controls.Add(this.removeImageCheckBox);
+            this.panel1.Controls.Add(this.submitButton);
+            this.panel1.Controls.Add(this.urlTextBox);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(784, 117);
+            this.panel1.TabIndex = 1;
+            // 
+            // submitButton
+            // 
+            this.submitButton.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.submitButton.Font = new System.Drawing.Font("PMingLiU", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.submitButton.Location = new System.Drawing.Point(697, 14);
+            this.submitButton.Name = "submitButton";
+            this.submitButton.Size = new System.Drawing.Size(75, 30);
+            this.submitButton.TabIndex = 2;
+            this.submitButton.Text = "轉換";
+            this.submitButton.UseVisualStyleBackColor = true;
+            this.submitButton.Click += new System.EventHandler(this.submitButton_Click);
+            // 
+            // urlTextBox
+            // 
+            this.urlTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.urlTextBox.Font = new System.Drawing.Font("Microsoft JhengHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.urlTextBox.Location = new System.Drawing.Point(77, 15);
+            this.urlTextBox.Name = "urlTextBox";
+            this.urlTextBox.Size = new System.Drawing.Size(605, 29);
+            this.urlTextBox.TabIndex = 1;
+            this.urlTextBox.WordWrap = false;
+            this.urlTextBox.KeyUp += new System.Windows.Forms.KeyEventHandler(this.urlTextBox_KeyUp);
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser1.Location = new System.Drawing.Point(0, 117);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(784, 20);
+            this.webBrowser1.TabIndex = 2;
+            this.webBrowser1.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.webBrowser1_DocumentCompleted);
+            this.webBrowser1.ProgressChanged += new System.Windows.Forms.WebBrowserProgressChangedEventHandler(this.webBrowser1_ProgressChanged);
+            // 
+            // removeImageCheckBox
+            // 
+            this.removeImageCheckBox.AutoSize = true;
+            this.removeImageCheckBox.Font = new System.Drawing.Font("Microsoft JhengHei", 12F);
+            this.removeImageCheckBox.Location = new System.Drawing.Point(16, 61);
+            this.removeImageCheckBox.Name = "removeImageCheckBox";
+            this.removeImageCheckBox.Size = new System.Drawing.Size(92, 24);
+            this.removeImageCheckBox.TabIndex = 3;
+            this.removeImageCheckBox.Text = "移除圖片";
+            this.removeImageCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // removeHyperlinkCheckBox
+            // 
+            this.removeHyperlinkCheckBox.AutoSize = true;
+            this.removeHyperlinkCheckBox.Font = new System.Drawing.Font("Microsoft JhengHei", 12F);
+            this.removeHyperlinkCheckBox.Location = new System.Drawing.Point(131, 61);
+            this.removeHyperlinkCheckBox.Name = "removeHyperlinkCheckBox";
+            this.removeHyperlinkCheckBox.Size = new System.Drawing.Size(108, 24);
+            this.removeHyperlinkCheckBox.TabIndex = 3;
+            this.removeHyperlinkCheckBox.Text = "移除超連結";
+            this.removeHyperlinkCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 101);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.panel1);
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "自動網頁轉換文件";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox urlTextBox;
+        private System.Windows.Forms.FontDialog fontDialog1;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.Button submitButton;
+        private System.Windows.Forms.CheckBox removeHyperlinkCheckBox;
+        private System.Windows.Forms.CheckBox removeImageCheckBox;
+    }
+}
+
---
+++ AutoWebBrowserContentCapture/MainForm.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+
+using Microsoft.Office.Core;
+using Word = Microsoft.Office.Interop.Word;
+using mshtml;
+
+namespace AutoWebBrowserContentCapture
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+
+            webBrowser1.ScriptErrorsSuppressed = true;
+
+            ServicePointManager.ServerCertificateValidationCallback += (sender, cert, chain, sslPolicyErrors) => true;
+            System.Net.ServicePointManager.SecurityProtocol = System.Net.SecurityProtocolType.Tls12;
+            // http://www.codeproject.com/Articles/31163/Suppressing-Hosted-WebBrowser-Control-Dialogs
+
+        }
+
+        private void urlTextBox_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                Navigate();
+            }
+        }
+
+        private void submitButton_Click(object sender, EventArgs e)
+        {
+            Navigate();
+        }
+
+        void Navigate()
+        {
+            try
+            {
+                Encoding encode = Encoding.UTF8;
+                HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create(urlTextBox.Text);
+                request.Method = "GET";
+                request.UserAgent = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome Safari/537.36";
+                request.KeepAlive = false;
+                IAsyncResult iar1 = request.BeginGetResponse(null, null);
+                try
+                {
+                    using (HttpWebResponse response = (HttpWebResponse)request.EndGetResponse(iar1))
+                    {
+                        if (response.StatusCode == HttpStatusCode.OK || response.StatusCode == HttpStatusCode.Created)
+                        {
+                            string temp = Path.GetTempFileName();
+                            BinaryReader br = new BinaryReader(response.GetResponseStream());
+                            byte[] buffer = new byte[4096];
+                            MemoryStream ms = new MemoryStream();
+                            int n = 0;
+                            do
+                            {
+                                n = br.Read(buffer, 0, buffer.Length);
+                                ms.Write(buffer, 0, n);
+                            } while (n > 0);
+                            br.Dispose();
+                            buffer = ms.ToArray();
+                            ms.Close();
+
+                            String html = Encoding.ASCII.GetString(buffer);
+                            try
+                            {
+                                Match match = Regex.Match(html, @"<meta[^>]*charset[^>]*>", RegexOptions.IgnoreCase | RegexOptions.Singleline);
+                                if (match.Success)
+                                {
+                                    string[] ss = match.Value.Split(new string[] { "charset=" }, StringSplitOptions.RemoveEmptyEntries);
+                                    string charset = ss[ss.Length - 1].Split(new char[] { '"', ' ' }, StringSplitOptions.RemoveEmptyEntries)[0];
+                                    encode = Encoding.GetEncoding(charset);
+                                }
+                            }
+                            catch
+                            {
+                            }
+
+
+                            html = encode.GetString(buffer);
+                            // http://bytes.com/topic/c-sharp/answers/228303-using-mshtml-parsing-html-files-c
+                            try
+                            {
+                                // http://stackoverflow.com/questions/2505957/using-regex-to-remove-script-tags
+                                // http://go4answers.webhost4life.com/Example/regex-match-body-tag-help-111595.aspx
+                                Match match = Regex.Match(html, @"<body[^>]*>(.*?)</body>", RegexOptions.IgnoreCase | RegexOptions.Singleline);
+                                if (match.Success)
+                                {
+                                    html = match.Value;
+                                    HTMLDocument doc = new HTMLDocument();
+                                    IHTMLDocument2 doc2 = (IHTMLDocument2)doc;
+                                    doc2.write(new object[] { html });
+
+                                    IHTMLDOMNode dom = (IHTMLDOMNode)doc2;
+
+                                    IHTMLElementCollection scripts = ((IHTMLElement2)doc2.body).getElementsByTagName("script");                                    
+                                    foreach (IHTMLElement script in scripts)
+                                    {
+                                        // http://stackoverflow.com/questions/554233/removing-htmlelement-objects-programmatically-using-c-sharp
+                                        IHTMLDOMNode node = script as IHTMLDOMNode;
+                                        node.parentNode.removeChild(node);
+                                    }
+
+                                    IHTMLElementCollection imgs = ((IHTMLElement2)doc2.body).getElementsByTagName("img");                                    
+                                    foreach (IHTMLElement img in imgs)
+                                    {                                        
+                                        IHTMLDOMNode node = img as IHTMLDOMNode;
+                                        IHTMLElement el = img as IHTMLElement;
+                                        string src = el.getAttribute("src");                                        
+                                        if (removeImageCheckBox.Checked || !src.StartsWith("http"))
+                                        {
+                                            node.parentNode.removeChild(node);
+                                        }
+                                    }
+
+                                    if (removeHyperlinkCheckBox.Checked)
+                                    {
+                                        IHTMLElementCollection @as = ((IHTMLElement2)doc2.body).getElementsByTagName("a");
+                                        foreach (IHTMLElement a in @as)
+                                        {
+                                            IHTMLDOMNode node = a as IHTMLDOMNode;
+                                            node.parentNode.removeChild(node);
+                                        }
+                                    }
+
+                                    html = doc2.body.innerHTML;
+                                }
+                            }
+                            catch
+                            {
+                            }
+                            html = String.Format("<html><body>{0}</body></html>", html);
+                            File.WriteAllText(temp, html, encode);
+
+                            // http://stackoverflow.com/questions/5362591/how-to-display-the-string-html-contents-into-webbrowser-control
+                            //webBrowser1.DocumentText = html;
+
+                            // http://www.c-sharpcorner.com/uploadfile/d2dcfc/convert-html-to-word-then-word-to-pdf-with-C-Sharp/
+                            object missing = System.Reflection.Missing.Value;
+                            Word.Application word = new Word.Application();
+                            // http://bytes.com/topic/c-sharp/answers/243445-word-interop-quickly-inserting-html-files-into-word-doc
+                            word.DisplayAlerts = Word.WdAlertLevel.wdAlertsNone;                            
+                            Word.Document document = word.Documents.Add(ref missing, ref missing, ref missing, ref missing);
+                            // http://www.cprogramdevelop.com/3434569/
+                            document.ActiveWindow.View.Type = Word.WdViewType.wdWebView;
+                            //word.Options.Pagination = false;
+                            //word.ScreenUpdating = false;
+                            word.Visible = true;
+
+                            // http://charliedigital.com/2010/09/29/inserting-html-into-word-documents/
+                            // http://msdn.microsoft.com/en-us/library/bb217015%28office.12%29.aspx
+                            // http://msdn.microsoft.com/en-us/library/office/ff191763(v=office.15).aspx
+                            document.Range().InsertFile(temp, missing, false, false, false);
+                            //Clipboard.SetText(html, TextDataFormat.Text);                            
+                            //word.Selection.PasteSpecial(missing, missing, missing, missing, Word.WdPasteDataType.wdPasteHTML, missing, missing);
+                            
+                            word.Activate();
+
+                        }
+                        else
+                        {
+                        }
+                    }
+                }
+                catch (WebException e4)
+                {
+                    throw e4;
+                }
+                catch (Exception e2)
+                {
+                    throw e2;
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+        }
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+
+        }
+
+        private void webBrowser1_ProgressChanged(object sender, WebBrowserProgressChangedEventArgs e)
+        {
+
+        }
+    }
+}
